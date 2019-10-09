5f2424277ed0779769d9ea56df106fbb9040e7b2
Keefe Laporte <keefelaporte2019@gmail.com.com>
2019-10-08 21:10:39 -0400

    null == undefined, and added &&

--- hazyCalculator.js
+++ hazyCalculator.js
@@ -3,16 +3,16 @@ function isSkippedValue(value) {
 }
 
 function isNumericValue(value) {
-  return !isNaN(value)
+  return !isNaN(value) && value !== ''
 }
 
 function isNothingValue(value) {
-  return value === null
+  return value == null 
 }
 
 function isAcceptableValue(value) {
   const operators = ['+', '-', '*', '/']
-  return typeof value === Number || operators.includes(value)
+  return typeof Number(value) === 'number' || operators.includes(value)
 }
 
 function performCalculationStep(firstOperand, operator, secondOperand) {
